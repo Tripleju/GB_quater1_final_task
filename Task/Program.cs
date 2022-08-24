1c8a431d497ea19a2d29b4402520950e4a20eb64
July <filimonovajuly@gmail.com>
2022-08-24 19:19:07 +0300

    добавлен итоговый вариант программы

--- Task/Program.cs
+++ Task/Program.cs
@@ -8,68 +8,74 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
-//Массив задается на старте выполнения алгоритма
 
-string[] CountNum()
+//Ввод данных с клавиатуры и сохранение в массив
+string[] InsertMass()
 {
     int count = 0;
     string word;
     string[] arr = new string[count];
+    string[] arr2;
+
     while (true)
     {
-        for (int i = 0; i < count; i++)
+        Console.Write("Введите что-нибудь: ");
+        word = Console.ReadLine();
+        if (word == "") return arr;
+        else
         {
-            Console.Write("Введите любое число: ");
-            word = Console.ReadLine();
-
-            if (word == "") return arr;
-            else
+            count++;
+            arr2 = new string[count];
+            for (int i = 0; i < arr.Length; i++)
             {
-                count++;
-                arr[i] = word;
-
+                arr2[i] = arr[i];
             }
-
+            arr2[count - 1] = word;
+            arr = arr2;
         }
     }
-
 }
 
-
-
-
-// string[] arr = { "hello", "2", "world", ":-)" };
-
+//Печать заданного массива (не обязательная часть)
 void PrintArray(string[] array)
 {
     int count = array.Length;
-    Console.Write($"['{array[0]}', ");
-    for (int i = 1; i < count - 1; i++)
-    {
-        Console.Write($"'{array[i]}', ");
-    }
 
-    Console.Write($"'{array[count - 1]}']");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
 
     Console.WriteLine();
 }
+// Из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа
+void ShortArray(string[] array)
+{
+    int count = array.Length;
+    Console.Write($"[");
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i].Length < 4)
+            Console.Write($"'{array[i]}' ");
+    }
+    Console.Write($"]");
+    Console.WriteLine();
+}
+//Вывод варианта, где массив задается на старте выполнения алгоритма
 
-// void ShortArray(string[] array)
-// {
-//     int count = array.Length;
-//     Console.Write($"[");
-//     for (int i = 0; i < count; i++)
-//     {
-//         if (array[i].Length < 4)
-//             Console.Write($"'{array[i]}', ");
-//     }
-//     Console.Write($"]");
-//     Console.WriteLine();
-// }
+string[] arrtest1 = { "hello", "2", "world", ":-)" };
+PrintArray(arrtest1);
+ShortArray(arrtest1);
 
-// PrintArray(arr);
-// ShortArray(arr);
+string[] arrtest2 = { "1234", "1567", "-2", "computer science" };
+PrintArray(arrtest2);
+ShortArray(arrtest2);
 
+string[] arrtest3 = { "Russia", "Denmark", "Kazan" };
+PrintArray(arrtest3);
+ShortArray(arrtest3);
 
-CountNum();
-PrintArray(CountNum());
+//Вывод варианта, где массив вводится с клавиатуры
+
+ShortArray(InsertMass());
