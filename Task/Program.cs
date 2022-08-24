1427108daeacdda671c97e04a42200ba7a276c9f
July <filimonovajuly@gmail.com>
2022-08-24 12:01:09 +0300

    добавлен вариант 1 решения

---
+++ Task/Program.cs
@@ -0,0 +1,43 @@
+﻿// **Задача**: Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+//  лучше обойтись исключительно массивами.
+
+// **Примеры**:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+//Массив задается на старте выполнения алгоритма
+
+string[] arr = { "hello", "2", "world", ":-)" };
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    Console.Write($"['{array[0]}', ");
+    for (int i = 1; i < count - 1; i++)
+    {
+        Console.Write($"'{array[i]}', ");
+    }
+
+    Console.Write($"'{array[count - 1]}']");
+
+    Console.WriteLine();
+}
+
+void ShortArray(string[] array)
+{
+    int count = array.Length;
+    Console.Write($"[");
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i].Length < 4)
+            Console.Write($"'{array[i]}', ");
+    }
+    Console.Write($"]");
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+ShortArray(arr);
